7e8fd5c75a0a2b1d6c5644db68f0fcca5778ce62
Daggerbot <daggerbot@gmail.com>
2015-05-03 20:03:30 -0700

    Removed public OpenGL typedefs which could have caused problems with glob imports.

--- src/lib.rs
+++ src/lib.rs
@@ -12,13 +12,11 @@ extern crate libc;
 
 use libc::{
   c_char,
+  c_int,
+  c_uchar,
+  c_uint,
   c_void,
 };
-
-pub type GLboolean = libc::c_uchar;
-pub type GLenum = libc::c_uint;
-pub type GLint = libc::c_int;
-pub type GLsizei = libc::c_int;
 
 
 //
@@ -27,17 +25,17 @@ pub type GLsizei = libc::c_int;
 
 
 shared_library!(OsMesa, "libOSMesa.so",
-  pub fn OSMesaColorClamp (enable: GLboolean),
-  pub fn OSMesaCreateContext (format: GLenum, sharelist: OSMesaContext) -> OSMesaContext,
-  pub fn OSMesaCreateContextExt (format: GLenum, depthBits: GLint, stencilBits: GLint, accumBits: GLint, sharelist: OSMesaContext) -> OSMesaContext,
+  pub fn OSMesaColorClamp (enable: c_uchar),
+  pub fn OSMesaCreateContext (format: c_uint, sharelist: OSMesaContext) -> OSMesaContext,
+  pub fn OSMesaCreateContextExt (format: c_uint, depthBits: c_int, stencilBits: c_int, accumBits: c_int, sharelist: OSMesaContext) -> OSMesaContext,
   pub fn OSMesaDestroyContext (ctx: OSMesaContext),
-  pub fn OSMesaGetColorBuffer (c: OSMesaContext, width: *mut GLint, height: *mut GLint, format: *mut GLint, buffer: *mut *mut c_void) -> GLboolean,
+  pub fn OSMesaGetColorBuffer (c: OSMesaContext, width: *mut c_int, height: *mut c_int, format: *mut c_int, buffer: *mut *mut c_void) -> c_uchar,
   pub fn OSMesaGetCurrentContext () -> OSMesaContext,
-  pub fn OSMesaGetDepthBuffer (c: OSMesaContext, width: *mut GLint, height: *mut GLint, bytesPerValue: *mut GLint, buffer: *mut *mut c_void) -> GLboolean,
-  pub fn OSMesaGetIntegerv (pname: GLint, value: *mut GLint),
+  pub fn OSMesaGetDepthBuffer (c: OSMesaContext, width: *mut c_int, height: *mut c_int, bytesPerValue: *mut c_int, buffer: *mut *mut c_void) -> c_uchar,
+  pub fn OSMesaGetIntegerv (pname: c_int, value: *mut c_int),
   pub fn OSMesaGetProcAddress (funcName: *const c_char) -> OSMESAproc,
-  pub fn OSMesaMakeCurrent (ctx: OSMesaContext, buffer: *mut c_void, _type: GLenum, width: GLsizei, height: GLsizei) -> GLboolean,
-  pub fn OSMesaPixelStore (pname: GLint, value: GLint),
+  pub fn OSMesaMakeCurrent (ctx: OSMesaContext, buffer: *mut c_void, _type: c_uint, width: c_int, height: c_int) -> c_uchar,
+  pub fn OSMesaPixelStore (pname: c_int, value: c_int),
 );
 
 
@@ -60,22 +58,22 @@ pub type OSMESAproc = Option<unsafe extern "C" fn ()>;
 
 
 // context formats
-pub const OSMESA_BGRA: GLenum = 0x0001;
-pub const OSMESA_ARGB: GLenum = 0x0002;
-pub const OSMESA_BGR: GLenum = 0x0004;
-pub const OSMESA_RGB_565: GLenum = 0x0005;
-pub const OSMESA_COLOR_INDEX: GLenum = 0x1900;
-pub const OSMESA_RGB: GLenum = 0x1907;
-pub const OSMESA_RGBA: GLenum = 0x1908;
+pub const OSMESA_BGRA: c_uint = 0x0001;
+pub const OSMESA_ARGB: c_uint = 0x0002;
+pub const OSMESA_BGR: c_uint = 0x0004;
+pub const OSMESA_RGB_565: c_uint = 0x0005;
+pub const OSMESA_COLOR_INDEX: c_uint = 0x1900;
+pub const OSMESA_RGB: c_uint = 0x1907;
+pub const OSMESA_RGBA: c_uint = 0x1908;
 
 // OSMesaGetIntegerv
-pub const OSMESA_WIDTH: GLint = 0x0020;
-pub const OSMESA_HEIGHT: GLint = 0x0021;
-pub const OSMESA_FORMAT: GLint = 0x0022;
-pub const OSMESA_TYPE: GLint = 0x0023;
-pub const OSMESA_MAX_WIDTH: GLint = 0x0024;
-pub const OSMESA_MAX_HEIGHT: GLint = 0x0025;
+pub const OSMESA_WIDTH: c_int = 0x0020;
+pub const OSMESA_HEIGHT: c_int = 0x0021;
+pub const OSMESA_FORMAT: c_int = 0x0022;
+pub const OSMESA_TYPE: c_int = 0x0023;
+pub const OSMESA_MAX_WIDTH: c_int = 0x0024;
+pub const OSMESA_MAX_HEIGHT: c_int = 0x0025;
 
 // OSMesaPixelStore
-pub const OSMESA_ROW_LENGTH: GLint = 0x0010;
-pub const OSMESA_Y_UP: GLint = 0x0011;
+pub const OSMESA_ROW_LENGTH: c_int = 0x0010;
+pub const OSMESA_Y_UP: c_int = 0x0011;
